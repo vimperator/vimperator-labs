7165d81bbbca64fae843036d3469287547f0c357
Doug Kearns <dougkearns@gmail.com>
2007-09-07 08:06:39 +0000

    minor improvement to 'popups' help wording

--- chrome/content/vimperator/options.js
+++ chrome/content/vimperator/options.js
@@ -465,7 +465,7 @@ function Options() //{{{
             short_help: "Where to show requested popup windows",
             help: "Define where to show requested popup windows. Does not apply to windows which are opened by middle clicking a link, they always open in a new tab. " +
                   "Possible values:<br/><ul>" +
-                  "<li><b>0</b>: Force to open in the current tab (NOTE: this can make some web sites stop from working correctly!)</li>" +
+                  "<li><b>0</b>: Force to open in the current tab (NOTE: this can stop some web sites from working correctly!)</li>" +
                   "<li><b>1</b>: Always open in a new tab</li>" +
                   "<li><b>2</b>: Open in a new window if it has a specific requested size (default in Firefox)</li>"+
                   "<li><b>3</b>: Always open in a new window</li></ul>" +
