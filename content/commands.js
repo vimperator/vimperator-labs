323a5f5e33eddd264e512c9ffa3a1aa1bc499746
Doug Kearns <dougkearns@gmail.com>
2007-10-30 09:33:51 +0000

    merge improved :version command

--- content/commands.js
+++ content/commands.js
@@ -1758,7 +1758,8 @@ vimperator.Commands = function() //{{{
             if (special)
                 vimperator.open("about:");
             else
-                vimperator.echo("Vimperator " + vimperator.version);
+                vimperator.echo(":" + vimperator.util.escapeHTML(vimperator.commandline.getCommand()) +
+                                "\nVimperator " + vimperator.version + " running on:\n" + navigator.userAgent);
         },
         {
             usage: ["ve[rsion][!]"],
