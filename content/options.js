19e342bc3ab003c63a5587dc027e87db08683c17
Martin Stubenschrott <stubenschrott@gmx.net>
2007-11-19 18:42:02 +0000

    changed default next/prev patterns slightly

--- content/options.js
+++ content/options.js
@@ -605,8 +605,8 @@ vimperator.Options = function () //{{{
         {
             shortHelp: "Patterns to use when guessing the 'next' page in a document sequence",
             help: "Each pattern, in order, is matched against all links in the page with the first match being used.<br/>" +
-            "The patterns are case insensitive.",
-            defaultValue: "\\bnext,^>$"
+            "The patterns are case insensitive regular expressions.",
+            defaultValue: "\\bnext,^>$,^>>$"
         }
     ));
     optionManager.add(new vimperator.Option(["pageinfo", "pa"], "charlist",
@@ -662,8 +662,8 @@ vimperator.Options = function () //{{{
         {
             shortHelp: "Patterns to use when guessing the 'previous' page in a document sequence",
             help: "Each pattern, in order, is matched against all links in the page with the first match being used.<br/>" +
-            "The patterns are case insensitive.",
-            defaultValue: "\\bprev,previous\\b,^<$"
+            "The patterns are case insensitive regular expressions.",
+            defaultValue: "\\bprev|previous\\b,^<$,^<<$"
         }
     ));
     optionManager.add(new vimperator.Option(["scroll", "scr"], "number",
