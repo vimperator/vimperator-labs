d2021031fe1de3b90da5f8bdbde1cb716533d358
Daniel Bainton <dpb@driftaway.org>
2008-03-20 16:00:13 +0000

    Fixing a typo in the liberator.getWindows() function name

--- content/vim.js
+++ content/vim.js
@@ -903,7 +903,7 @@ const liberator = (function () //{{{
                 mainThread.processNextEvent(true);
         },
 
-        get windows()
+        getWindows: function()
         {
             var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
                      .getService(Components.interfaces.nsIWindowMediator);
