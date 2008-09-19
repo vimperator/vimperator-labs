97f9fa4132127ebcaceb2cb4db57a1fbbf23a3dc
Daniel Bainton <dpb@driftaway.org>
2008-09-19 11:19:32 +0000

    Fix io.createTempFile

--- content/io.js
+++ content/io.js
@@ -474,9 +474,11 @@ liberator.IO = function () //{{{
             var file = Components.classes["@mozilla.org/file/local;1"]
                                  .createInstance(Components.interfaces.nsILocalFile);
 
+            var tmpname = liberator.config.name.toLowerCase() + "-";
             if (window.content.document.location.hostname)
-                var tmpname = liberator.config.name.toLowerCase() + "-" +
-                    window.content.document.location.hostname + ".tmp";
+                tmpname += window.content.document.location.hostname;
+            tmpname += ".tmp";
+
             if (liberator.config.name == "Muttator")
                 tmpname = "mutt-ator-mail"; // to allow vim to :set ft=mail automatically
 
