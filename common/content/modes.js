9faab2c818eabf9efc03702469e85fe7b482345d
U-MAIN\anekos <anekos@main.(none)>
2008-12-04 22:12:20 +0900

    Fix: Issue of escaping from hint mode.

--- common/content/modes.js
+++ common/content/modes.js
@@ -180,12 +180,12 @@ const modes = (function () //{{{
             // if a main mode is set, the extended is always cleared
             if (typeof mainMode === "number")
             {
+                if (!silent && mainMode != main)
+                    handleModeChange(main, mainMode);
+
                 main = mainMode;
                 if (!extendedMode)
                     extended = modes.NONE;
-
-                if (!silent && mainMode != main)
-                    handleModeChange(main, mainMode);
             }
             if (typeof extendedMode === "number")
                 extended = extendedMode;
