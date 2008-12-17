994bc00a4f845b15fe321788aefb823111a47db7
Doug Kearns <dougkearns@gmail.com>
2008-12-18 03:26:22 +1100

    nsIWebNavigation -> Ci.nsIWebNavigation in liberator.open for Muttator

--- common/content/liberator.js
+++ common/content/liberator.js
@@ -1091,7 +1091,7 @@ const liberator = (function () //{{{
                 switch (where)
                 {
                     case liberator.CURRENT_TAB:
-                        getBrowser().loadURIWithFlags(url, nsIWebNavigation.LOAD_FLAGS_NONE, null, null, postdata);
+                        getBrowser().loadURIWithFlags(url, Ci.nsIWebNavigation.LOAD_FLAGS_NONE, null, null, postdata);
                         break;
 
                     case liberator.NEW_BACKGROUND_TAB:
