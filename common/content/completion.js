1a13d07e9f467e9c4acddb8536800333a391c218
Kris Maglione <maglione.k@gmail.com>
2008-12-20 18:40:34 -0500

    Unbreak :se! completion.

--- common/content/completion.js
+++ common/content/completion.js
@@ -1651,7 +1651,7 @@ function Completion() //{{{
             context.anchored = false;
             context.title = ["Firefox Preference", "Value"];
             context.keys = { text: function (item) item, description: function (item) options.getPref(item) };
-            context.completions = service["prefs"].getChildList("", { value: 0 });
+            context.completions = service["pref"].getChildList("", { value: 0 });
         },
 
         search: function search(context, noSuggest)
