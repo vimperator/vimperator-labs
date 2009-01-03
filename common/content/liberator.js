0e22f0e4fd0446effab8609ebc4b3070321700f4
Kris Maglione <maglione.k@gmail.com>
2009-01-02 19:13:11 -0500

    Make echoerr, throw Error in liberator.open

--- common/content/liberator.js
+++ common/content/liberator.js
@@ -1122,9 +1122,7 @@ const liberator = (function () //{{{
                         break;
 
                     default:
-                        // FIXME: should this really be a user-visible error? --djk
-                        liberator.echoerr("Exxx: Invalid 'where' directive in liberator.open(...)");
-                        return false;
+                        throw Error("Invalid 'where' directive in liberator.open(...)");
                 }
             }
 
