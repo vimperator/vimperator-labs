61b6491b8b4ca6003166d6533d2518a7a1db26dc
Doug Kearns <dougkearns@gmail.com>
2009-05-24 23:26:15 +1000

    Add all.html for viewing all help in a single page.
    This is all really inside out. It would be much better to store the help
    in a single file and use asciidoc/docbook's capabilities to split it if
    desired.

--- vimperator/content/config.js
+++ vimperator/content/config.js
@@ -119,7 +119,7 @@ const config = { //{{{
         "pattern.html", "tabs.html", "hints.html", "map.html", "eval.html",
         "marks.html", "repeat.html", "autocommands.html", "print.html",
         "gui.html", "styling.html", "message.html", "developer.html",
-        "various.html", "index.html", "version.html"
+        "various.html", "version.html", "index.html", "all.html"
     ],
 
     get ignoreKeys() {
