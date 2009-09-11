4d4da18b49303d54673a20a1271470e19c62ed6b
Martin Stubenschrott <stubenschrott@vimperator.org>
2009-09-11 22:27:15 +0200

    fix whitespace from last commit

--- common/content/editor.js
+++ common/content/editor.js
@@ -894,12 +894,12 @@ function Editor() //{{{
             if (!(config.isComposeWindow))
                 textBox = liberator.focus;
 
-			if (textBox.type == "password")
-			{
+            if (textBox.type == "password")
+            {
                 liberator.beep();
-			    liberator.echoerr("Cannot edit password fields");
-			    return false;
-			}
+                liberator.echoerr("Cannot edit password fields");
+                return false;
+            }
 
             let text = ""; // XXX
             if (textBox)
