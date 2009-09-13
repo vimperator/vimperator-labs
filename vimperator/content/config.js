a2a2f17e6263432dcb124097a083847fef5c0c9f
Martin Stubenschrott <stubenschrott@vimperator.org>
2009-09-13 17:31:06 +0200

    remove 't' from default 'complete' in preparation for 2.2. We have yet to decide whether we even want to this functionality in :open or maybe just add a super smart :goto)

--- vimperator/content/config.js
+++ vimperator/content/config.js
@@ -34,7 +34,7 @@ const config = { //{{{
     /*** optional options, there are checked for existence and a fallback provided  ***/
     features: ["bookmarks", "hints", "history", "marks", "quickmarks", "sanitizer", "session", "tabs", "tabs_undo", "windows"],
     defaults: {
-        complete: "stlf",
+        complete: "slf",
         guioptions: "rb",
         showtabline: 2,
         titlestring: "Vimperator"
