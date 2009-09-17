52378d8a9f64d9800fe7453fbf60b7154c79e6e4
Kris Maglione <maglione.k@gmail.com>
2009-09-17 13:58:19 -0400

    Fix :tabdetach

--- common/content/tabs.js
+++ common/content/tabs.js
@@ -590,7 +590,7 @@ function Tabs() //{{{
             "Detach current tab to its own window",
             function ()
             {
-                if (tabs.count == 1)
+                if (tabs.count == 1 && !userContext.ImADeveloper_IKnowWhatImDoing_LeaveMeTheFuckAlone)
                     return void liberator.echoerr("Can't detach the last tab");
 
                 tabs.detachTab(null);
