57a6db23bec476c6b93ae5369c8337e2d68cc357
anekos <anekos@snca.net>
2010-02-08 23:26:44 +0900

    Add "history" to the "-complete" option of ":com[mand]".

--- common/content/commands.js
+++ common/content/commands.js
@@ -947,11 +947,11 @@ const Commands = Module("commands", {
             bookmark: "bookmark", buffer: "buffer", color: "colorScheme",
             command: "command", dialog: "dialog", dir: "directory",
             environment: "environment", event: "autocmdEvent", file: "file",
-            help: "help", highlight: "highlightGroup", javascript: "javascript",
-            macro: "macro", mapping: "userMapping", menu: "menuItem",
-            option: "option", preference: "preference", search: "search",
-            shellcmd: "shellCommand", sidebar: "sidebar", url: "url",
-            usercommand: "userCommand"
+            help: "help", highlight: "highlightGroup", history: "history",
+            javascript: "javascript", macro: "macro", mapping: "userMapping",
+            menu: "menuItem", option: "option", preference: "preference",
+            search: "search", shellcmd: "shellCommand", sidebar: "sidebar",
+            url: "url", usercommand: "userCommand"
         };
 
         // TODO: Vim allows commands to be defined without {rep} if there are {attr}s
