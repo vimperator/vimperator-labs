088c4138962a3a572b55ef805b6763acf3f4f619
Kei Shiratsuchi <kei.shiratsuchi@gmail.com>
2010-10-05 21:59:05 +0200

    improved gu behavior

--- common/content/browser.js
+++ common/content/browser.js
@@ -155,29 +155,60 @@ const Browser = Module("browser", {
         mappings.add([modes.NORMAL], ["gu"],
             "Go to parent directory",
             function (count) {
-                function isDirectory(url) {
-                    if (/^file:\/|^\//.test(url)) {
-                        let file = io.File(url);
-                        return file.exists() && file.isDirectory();
+                function getParent(url, count) {
+                    function getParentPath(path) {
+                        if (!path)
+                            return;
+
+                        path = path.replace(/\/$/, "").replace(/^\/+/, "");
+                        if (path.indexOf("#") > 0)
+                            return path.replace(/#.*/, "");
+
+                        if (path.indexOf("?") > 0)
+                            return path.replace(/\?.*/, "");
+
+                        path = path.replace(/\/+$/, "");
+                        if (path.indexOf("/") > 0)
+                            return path.replace(/\/[^\/]*$/,"/");
                     }
-                    else {
-                        // for all other locations just check if the URL ends with /
-                        return /\/$/.test(url);
+
+                    function getParentHost(host) {
+                        if (!/\./.test(host) || /^[0-9+.:]+$/.test(host))
+                            return host;
+
+                        let hostSuffix = "";
+                        let x = host.lastIndexOf(":");
+                        if (x > 0) {
+                            hostSuffix = host.substr(x);
+                            host = host.substr(0, x);
+                        }
+                        hostSuffix = host.substr(host.length - 6) + hostSuffix;
+                        host = host.substr(0, host.length - 6);
+                        return host.replace(/[^.]*\./, "") + hostSuffix;
                     }
+
+                    let parent = url;
+                    let regexp = new RegExp("([a-z]+:///?)([^/]*)(/.*)");
+                    let [, scheme, host, path] = regexp.exec(url);
+                    path = path.replace(/\/$/, "").replace(/^\/+/, "");
+                    for (let i = 0; i < count; i++) {
+                        if (path) {
+                            if (path = getParentPath(path))
+                                parent = scheme + host + "/" + path;
+                            else 
+                                parent = scheme + host + "/";
+                        }
+                        else {
+                            host = getParentHost(host);
+                            parent = scheme + host + "/";
+                        }
+                    }
+                    return parent;
                 }
 
                 if (count < 1)
                     count = 1;
-
-                // XXX
-                let url = buffer.URL;
-                for (let i = 0; i < count; i++) {
-                    if (isDirectory(url))
-                        url = url.replace(/^(.*?:)(.*?)([^\/]+\/*)$/, "$1$2/");
-                    else
-                        url = url.replace(/^(.*?:)(.*?)(\/+[^\/]+)$/, "$1$2/");
-                }
-                url = url.replace(/^(.*:\/+.*?)\/+$/, "$1/"); // get rid of more than 1 / at the end
+                let url = getParent(buffer.URL, count);
 
                 if (url == buffer.URL)
                     liberator.beep();
